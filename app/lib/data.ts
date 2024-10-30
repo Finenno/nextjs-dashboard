60018675725899fcd1eadc25edbaf2197f322c82
Artem Mitkin <butfineno@gmail.com>
2024-10-30 23:51:38 +0300

    Seven chapter

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,7 +9,17 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+
 export async function fetchRevenue() {
+  // Fetch the last 5 invoices, sorted by date
+const data = await sql<LatestInvoiceRaw>`
+SELECT invoices.amount, customers.name, customers.image_url, customers.email
+FROM invoices
+JOIN customers ON invoices.customer_id = customers.id
+ORDER BY invoices.date DESC
+LIMIT 5`;
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
